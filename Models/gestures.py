434ba619b8aa5b34773b8e8c497b4073c0e38ae0
Kartik Sharma <ks.kartik1610@gmail.com>
2024-05-18 01:10:32 +0530

    open close gestures added

--- Models/gestures.py
+++ Models/gestures.py
@@ -11,15 +11,21 @@ mp_draw = mp.solutions.drawing_utils
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
 
-# Variables for controlling the mouse
+# Variables for controlling the mouse and gestures
 prev_x, prev_y = 0, 0
 dragging = False
 gesture_start_time = None
+fist_closed = False
 
 def get_finger_tips(landmarks):
-    index_finger_tip = landmarks[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-    thumb_tip = landmarks[mp_hands.HandLandmark.THUMB_TIP]
-    return index_finger_tip, thumb_tip
+    finger_tips = [
+        landmarks[mp_hands.HandLandmark.INDEX_FINGER_TIP],
+        landmarks[mp_hands.HandLandmark.MIDDLE_FINGER_TIP],
+        landmarks[mp_hands.HandLandmark.RING_FINGER_TIP],
+        landmarks[mp_hands.HandLandmark.PINKY_TIP],
+        landmarks[mp_hands.HandLandmark.THUMB_TIP]
+    ]
+    return finger_tips
 
 def get_distance(point1, point2):
     return ((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2) ** 0.5
@@ -36,7 +42,10 @@ while True:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            index_tip, thumb_tip = get_finger_tips(hand_landmarks.landmark)
+            finger_tips = get_finger_tips(hand_landmarks.landmark)
+            tip_distances = [get_distance(finger_tips[i], finger_tips[i - 1]) for i in range(1, len(finger_tips))]
+
+            index_tip, thumb_tip = finger_tips[0], finger_tips[4]
             index_finger_x, index_finger_y = int(index_tip.x * img.shape[1]), int(index_tip.y * img.shape[0])
             thumb_x, thumb_y = int(thumb_tip.x * img.shape[1]), int(thumb_tip.y * img.shape[0])
             
@@ -77,9 +86,20 @@ while True:
                 gesture_start_time = time.time()
                 prev_x, prev_y = index_finger_x, index_finger_y
 
+            # Fist detection
+            is_fist_closed = all(d < 0.1 for d in tip_distances)
+
+            if is_fist_closed and not fist_closed:
+                print("PDF closed")
+                fist_closed = True
+            elif not is_fist_closed and fist_closed:
+                print("PDF opened")
+                fist_closed = False
+
     cv2.imshow("Gesture Controlled Virtual Mouse", img)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows()
+ 

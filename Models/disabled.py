938950dbcf16e47fff90c425504fe84f99970e5f
prakhar1535 <prakharsharma1535@gmail.com>
2024-05-19 13:59:06 +0530

    com

--- Models/disabled.py
+++ Models/disabled.py
@@ -8,7 +8,7 @@ cap=cv2.VideoCapture(0)
 # cap=set(3, 640)
 # cap=set(4, 480)
 
-model=YOLO(r'D:\deeplearning\vihaaan\gestures.pt')
+model=YOLO(r'/home/pyarakhar/Downloads/IDCS-dev/Models/gestures.pt')
 classNames = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'afraid', 'afternoon',
                'agree', 'angry', 'animal', 'b', 'baby', 'bad', 'bean', 'bird', 'book', 'bored', 
                'boy', 'bread', 'brother', 'butter', 'bye', 'c', 'cake', 'cat', 'chef', 'chicken', 'chocolate', 'circle', 'close hand', 'coco cola', 'coffee', 
@@ -22,7 +22,7 @@ classNames = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'afraid', '
                   , 'j', 'k', 'l', 'love', 'm', 'man', 'may', 'meet', 'milk', 'mine', 'monday', 'more', 
                'morning', 'mother', 'n', 'nervous', 'new', 'nice to meet you', 'night', 'no', 'none',
                  'o', 'okay', 'orange juice', 'p', 'paper', 'party', 'person', 'pig', 'pizza', 'please', 'practice', 'promise', 'proud', 'pumpkin', 'q', 'quiet', 'quite', 'r', 'rainbow', 
-                 'repeat', 's', 'sad', 'saturday', 'school', 'sheep', 'shocked', 'sister', 'sit down', 'son', 'sorry', 'stop', 'stressed', 'sugar', 'sunday', 
+                 'repeat', 's', 'sad', 'saturday', 'school', 'sheep', 'shocked', 'sister', 'sit down', 'son', 'stressed', 'sugar', 'sunday', 
                  't', 'tea', 'tennis', 'tent', 'thank you', 'thanks', 'thursday', 'tie', 'tired', 'to go', 'to sit', 'to wash', 'tuesday', 'u', 'v', 'w', 'walk', 
                  'wednesday', 'welcome', 'what', 'where', 'woman', 'worried', 'x', 'y', 'year', 'yes', 'z']
 while True:
